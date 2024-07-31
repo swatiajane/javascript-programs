72cbdcb9165ecc8c7bc27bc9254d910145a967d7
swatiajane <swatiajane66@gmail.com>
2024-07-31 23:06:49 +0530

    JsAssignment1
    JsAssignment1

---
+++ JsAss1.js
@@ -0,0 +1,33 @@
+console.log("=============1===============");
+var myFullName;
+console.log("My Full Name is Before initialization: ", myFullName);
+myFullName="swati sachin";
+console.log("My full Name is After initialization:",myFullName);
+myFullName="swati sachin ajane";
+console.log("After updating My full Name is:",myFullName);
+
+console.log("=============2===============");
+myLove="I Love Only Js"
+console.log(myLove);
+
+console.log("=============3===============");
+firstName="My firstName is : Swati"
+console.log(firstName);
+LastName="My LasttName is : Ajane"
+console.log(LastName);
+age="My age is : 20"
+console.log(age);
+address="address : pune"
+console.log(address);
+pincode="pincode : 432107"
+console.log(pincode);
+gender="gender : Female"
+console.log(gender);
+marriege="marriege status is : False"
+console.log(marriege);
+
+
+
+
+
+
---
+++ JsAss2.js
@@ -0,0 +1,4 @@
+var bank;
+console.log("Bank name is: ", bank);
+var bankTypeOf = typeof bank;
+console.log("DataType of Bank is: ", bankTypeOf);
