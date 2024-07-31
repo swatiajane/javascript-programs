2c76cc5dbc09523ef846d676c48b6c910f9ae05e
swatiajane <swatiajane66@gmail.com>
2024-07-31 20:53:18 +0530

    Javascript Variables
    Javascript Variables

---
+++ 01-variable.js
@@ -0,0 +1,14 @@
+// Variable Declaration
+var firstName;
+console.log(firstName);
+
+// Variable Initialization
+firstName = "Jenny";
+console.log(firstName);
+
+//Variable Declaration and Initialization
+var lastName = "Gates";
+console.log(lastName);
+//  Variable Updates
+firstName = "Jennifer"
+console.log(firstName);
---
+++ ass.js
@@ -0,0 +1,14 @@
+
+
+//Variable Declaration and Initialization
+cityName = "my city name is: pune";
+console.log(cityName);
+//  Variable Updates
+friendName = "my best friend name is: Anjali";
+console.log(friendName);
+countryName = "my country name is: India";
+console.log(countryName);
+schoolName = "my school name is: khadkeshwar school Bhalki";
+console.log(schoolName);
+collegeName = "my college name is: Annasaheb magar college";
+console.log(collegeName);
