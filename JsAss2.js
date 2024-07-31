618d81c7434c369ac25dfe4fe5e7bbfab5ea7f45
swatiajane <swatiajane66@gmail.com>
2024-07-31 23:36:24 +0530

    JsAssigment 2
    JsAssigment 2

--- JsAss2.js
+++ JsAss2.js
@@ -1,4 +1,29 @@
+console.log("============= 1 ===============");
 var bank;
 console.log("Bank name is: ", bank);
 var bankTypeOf = typeof bank;
 console.log("DataType of Bank is: ", bankTypeOf);
+var bank="SBI Bank"
+console.log("Bank name is:",bank)
+var bankTypeOf = typeof bank;
+console.log("Type of Datatype is:", bankTypeOf);
+console.log("============= 2 ===============");
+var marks = "95%";
+console.log("Mark is: ", marks);
+var marksTypeOf = typeof marks;
+console.log("Type of marks is:", marksTypeOf);
+console.log("============= 3 ===============");
+var isWorking = true;
+console.log("my working status is: ", isWorking);
+var isWorkingTypeOf = typeof isWorking;
+console.log("Working type of is: ", isWorkingTypeOf);
+console.log("============= 4 ===============");
+var totalcount="Hundred and Seven";
+console.log("totalcount is: ", totalcount);
+var totalcountTypeOf = typeof totalcount;
+console.log("totalcount type of is: ", totalcountTypeOf);
+console.log("============= 5 ===============");
+var totalcount=107;
+console.log("totalcount is: ", totalcount);
+var totalcountTypeOf = typeof totalcount;
+console.log("totalcount type of is: ", totalcountTypeOf);
