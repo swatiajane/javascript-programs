c3e48910bb188d5b41be2e5880c65c62ebcf6e7f
swatiajane <swatiajane66@gmail.com>
2024-08-02 15:20:55 +0530

    funAss 2
    funAss 2

--- funAss1.js
+++ funAss1.js
@@ -1,28 +1,28 @@
-console.log("------------------------1--------------------------")
-function First(){
-    console.log("My First Fuction");
+console.log("------------------------1--------------------------");
+function First() {
+  console.log("My First Fuction");
 }
-First(); 
-function Second(){
-    console.log("My Second Fuction");
+First();
+function Second() {
+  console.log("My Second Fuction");
 }
-Second(); 
+Second();
 
-console.log("-------------------------2----------------------------")
+console.log("-------------------------2----------------------------");
 
-function personalDetails(firstName,lastName,collegeName){
-    console.log("my firstName is : Swati");
-    console.log("my lastname is : Ajane");
-    console.log("my collegeName is : AnnaSaheb Magar");
+function personalDetails(firstName, lastName, collegeName) {
+  console.log("my firstName is : Swati");
+  console.log("my lastname is : Ajane");
+  console.log("my collegeName is : AnnaSaheb Magar");
 }
-    personalDetails();
+personalDetails();
 
-    console.log("-------------------------3----------------------------")
-     
-    function addThreeValues(value1,value2,value3)
-    {
-        var result=value1+value2,value3
-        console.log("Addition is",result)
-    }
-    addThreeValues(10.23 ,600 ,40);
-    addThreeValues("Hello"  , "GoodMorning" );
+console.log("-------------------------3----------------------------");
+
+function addThreeValues(value1, value2, value3) {
+  var result = value1 + value2,
+    value3;
+  console.log("Addition is", result);
+}
+addThreeValues(10.23, 600, 40);
+addThreeValues("Hello", "GoodMorning");
---
+++ funAss2.js
@@ -0,0 +1,6 @@
+function bankDetails(bankName, accountNum, location, pincode) {
+  console.log(" Bank name is :CITI Bank ||","Account Number is:", 3456782345, " || Location is : pune" , "|| pincode is :",431202);
+  console.log(" Bank name is :Axis Bank ||","Account Number is:", 7856782345, " || Location is : Mumbai" , "|| pincode is :",441202);
+  console.log(" Bank name is :HDFC Bank ||","Account Number is:", 8956782345, " || Location is : pune" , "|| pincode is :",631202,);
+}
+bankDetails();
