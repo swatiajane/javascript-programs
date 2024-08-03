e1444c6c370dd35817c32da3c0065fd9c7b16d6e
swatiajane <swatiajane66@gmail.com>
2024-08-03 22:22:56 +0530

    strAss1
    strAss1

---
+++ strAss1.js
@@ -0,0 +1,8 @@
+var Company = "TCS";
+console.log("My Dream Company is:",Company );
+var hobbyone = "Reading"
+var hobbytwo = "Dancing"
+var hobbythree = "Drawing"
+console.log(`My hobbies are:${hobbyone},${hobbytwo},${hobbythree}`);
+let totalCharacters = hobbyone.length + hobbytwo.length + hobbythree.length;
+console.log("Total number of characters in hobby1, hobby2, and hobby3 is : " + totalCharacters);
