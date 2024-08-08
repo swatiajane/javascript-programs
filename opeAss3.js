2721066738187ec813e61fc4c5b640a89cfb0356
swatiajane <swatiajane66@gmail.com>
2024-08-08 13:48:30 +0530

    opeAss3
    opeAss3

--- opeAss3.js
+++ opeAss3.js
@@ -1,19 +1,19 @@
 let result;
 
-result='3' + 2;
-console.log(result) // "32"
+result = "3" + 2;
+console.log(result); // "32"
 
 console.log("====================================");
 
-result='3' + true;
-console.log(result) // "3true"
+result = "3" + true;
+console.log(result); // "3true"
 
 console.log("====================================");
 
-result='3' + undefined;
-console.log(result) // "3undefined"
+result = "3" + undefined;
+console.log(result); // "3undefined"
 
 console.log("====================================");
 
-result='3' + null;
-console.log(result) // "3null"
+result = "3" + null;
+console.log(result); // "3null"
